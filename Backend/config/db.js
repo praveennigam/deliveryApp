2b49c689d711c9174f72d430b30f25d51c958e85
praveen nigam <praveennigam1999@gmail.com>
2024-06-22 03:19:55 +0530

    fdhgffh

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -5,7 +5,11 @@ dotenv.config();
 
 export const connectDB = async() => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            connectTimeoutMS: 30000, // 30 seconds timeout
+        });
         console.log('Database connected successfully');
     } catch (error) {
         console.error('Error connecting to database:', error.message);
