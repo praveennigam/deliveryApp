13a90a9c4e2cbb52bff4b89c847b4d5c6cb68cea
praveen nigam <praveennigam1999@gmail.com>
2024-06-22 16:23:21 +0530

    link insta

--- Frontend/src/Components/Footer/Footer.jsx
+++ Frontend/src/Components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaInstagram, FaLinkedin } from "react-icons/fa"; // Ensure this line is correct
 import "./Footer.css";
 import { assets } from "../../assets/assets";
 
@@ -7,12 +8,23 @@ const Footer = () => {
     <div className="footer" id="footer">
       <div className="footer-content">
         <div className="footer-content-left">
-          <img src={assets.logo} className="logom" alt="" />
+          <img src={assets.logo} className="logom" alt="Logo" />
           <p>Discover, Dine, Delight</p>
           <div className="footer-social-icons">
-            <img src={assets.facebook_icon} alt="" />
-            <img src={assets.twitter_icon} alt="" />
-            <img src={assets.linkedin_icon} alt="" />
+            <a
+              href="https://www.instagram.com/_its_praveen_?igsh=MWR5a25yY2lycTRkNg%3D%3D&utm_source=qr"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram size={40} />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/impraveen1999?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin size={40} />
+            </a>
           </div>
         </div>
         <div className="footer-content-center">
@@ -20,21 +32,25 @@ const Footer = () => {
           <ul>
             <li>Home</li>
             <li>About us</li>
-            <li> Delivery</li>
-            <li>Privacy Policy </li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
           </ul>
         </div>
         <div className="footer-content-right">
           <h2>GET IN TOUCH</h2>
           <ul>
             <li>+91-910-948-1480</li>
-            <li>praveennigam1999@gmail.com </li>
+            <li>
+              <a href="mailto:praveennigam1999@gmail.com">
+                praveennigam1999@gmail.com
+              </a>
+            </li>
           </ul>
         </div>
       </div>
       <hr />
       <p className="footer-copyright">
-        Copyright 2024 @ By Praveen.com -All Rights Reserved.
+        Copyright 2024 @ By Praveen.com - All Rights Reserved.
       </p>
     </div>
   );
